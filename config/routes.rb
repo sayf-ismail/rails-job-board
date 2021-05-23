c978a2bcf4d035458bee24ae06c2ae514bc658fe
Sayf Ismail <sayfti@gmail.com>
2021-05-23 21:49:49 +0800

    SignUp and Login features

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  
-  
   get "jobs", to: "jobs#index"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
 
   # get "/", to: "main#index" ; same as below!
   root to: "main#index"
