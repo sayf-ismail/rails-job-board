236ab0789a62dc808e8e97572c7c8fc2b4198508
Sayf Ismail <sayfti@gmail.com>
2021-05-24 23:15:50 +0800

    password edit, geocoder gem added

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@
 Rails.application.routes.draw do
   get "jobs", to: "jobs#index"
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+  
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
 
   # get "/", to: "main#index" ; same as below!
